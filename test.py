342373830e2de6f97d87f468d213719a873905e2
Roger Rush <rogerjrush@gmail.com>
2016-02-28 00:20:56 -0800

    Parse inline and block cf units

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import pyparsing as pp
 
 # BNF
@@ -14,18 +16,19 @@ import pyparsing as pp
 
 
 
-at      = pp.Literal('@')  # pp.Word("@", exact=1)
-strlit  = pp.Word(pp.alphanums)
-tag     = at + strlit
+at     = pp.Literal('@')  # pp.Word("@", exact=1)
+lcb    = pp.Literal('{')
+rcb    = pp.Literal('}')
+strlit = pp.Word(pp.alphanums)
+tag    = at + strlit
 inline_content = pp.CharsNotIn('\n')
-# block_content =
-# block   =
-cfunit  = pp.LineStart() + pp.OneOrMore(tag) \
-                    + pp.OneOrMore(inline_content) + pp.LineEnd()
-
-
-
-doc     = pp.OneOrMore(cfunit)
+block_content  = pp.OneOrMore(pp.CharsNotIn(['{', '}']))
+inline = pp.OneOrMore(inline_content)
+block  = lcb + pp.OneOrMore(block_content) + rcb
+inline_unit = pp.OneOrMore(tag) + inline
+block_unit  = pp.OneOrMore(tag) + block
+cfunit = block_unit | inline_unit
+# cfdoc  = pp.OneOrMore(cfunit)
 
 # Code
 testList = [
@@ -33,6 +36,8 @@ testList = [
     '@idea make a superintelligent machine\n',
     '@quote "The best laid plans..." - RB\n',
     '@watch @steph scotland, pa\n',
+    '@ok {ant bee crow}',
+    '@todo { water tree, play with peps, holla }',
     '@todo {\nwater tree\nplay with peps\nholla\n}',
     # Not valid
     "", "1", "$*", "a_#"
@@ -45,6 +50,7 @@ def test(s):
         print ("  Matches: {0}".format(result))
     except pp.ParseException as x:
         print ("  No match: {0}".format(str(x)))
+    print ()
 
 def main():
     for text in testList:
