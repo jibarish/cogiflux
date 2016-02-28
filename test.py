d1602dbaaee8a91826da00da89e94cb8b0805ed1
Roger Rush <rogerjrush@gmail.com>
2016-02-28 01:17:13 -0800

    Improve testing

--- test.py
+++ test.py
@@ -20,7 +20,9 @@ import pyparsing as pp
 # cfdoc          ::= { cfunit }
 # ----------------------------------------------------
 
-# Defs
+#
+# Grammar
+#
 at             = pp.Literal('@')
 lcb            = pp.Literal('{')
 rcb            = pp.Literal('}')
@@ -35,20 +37,25 @@ block_unit     = pp.OneOrMore(tag) + block
 cfunit         = block_unit | inline_unit
 cfdoc          = pp.OneOrMore(cfunit)
 
+#
 # Testing
-testList = [
-    # Valid cfunits
-    '@idea make a superintelligent machine\n',
-    '@quote "The best laid plans..." - RB\n',
-    '@watch @steph scotland, pa\n',
-    '@ok {ant bee crow}',
-    '@todo { water tree, play with peps, holla }',
-    '@todo {\nwater tree\nplay with peps\nholla\n}',
-    # Not valid
-    "", "1", "$*", "a_#"
-]
+#
+def test_singles():
+    testlist = [
+        # Valid cfunits
+        '@idea make a superintelligent machine\n',
+        '@quote "The best laid plans..." - RB\n',
+        '@watch @steph scotland, pa\n',
+        '@ok {ant bee crow}',
+        '@todo { water tree, play with peps, holla }',
+        '@todo {\nwater tree\nplay with peps\nholla\n}',
+        # Not valid
+        "", "1", "$*", "a_#"
+    ]
+    for text in testlist:
+        test_single(text)
 
-def test(s):
+def test_single(s):
     print ("---Test for '{0}'".format(s))
     try:
         result = cfunit.parseString(s)
@@ -57,9 +64,23 @@ def test(s):
         print ("  No match: {0}".format(str(x)))
     print ()
 
+def test_file(filename):
+    with open(filename, 'r') as infile:
+        s = infile.read()
+
+    print ("---Test for '{0}'".format(s))
+    try:
+        result = cfdoc.parseString(s)
+        # print ("  Matches: {0}".format(result))
+        for each in result:
+            print ("  Matches: {0}".format(each))
+    except pp.ParseException as x:
+        print ("  No match: {0}".format(str(x)))
+    print ()
+
 def main():
-    for text in testList:
-        test(text)
+    # test_singles()
+    # test_file("input.cflux")
 
 if __name__ == "__main__":
     main()
